ca10a3ed12836dc0c1ee3c7be9032e9164b7515d
Reza Faisal <rezha.faizal567@gmail.com>
2021-11-21 22:18:57 +0800

    pertama

---
+++ resources/views/baru.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+@section('title')
+    baru
+@endsection
+@section('body')
+    <h3>Halaman baru</h3>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.master')
+@section('title')
+    rumah
+@endsection
+@section('body')
+    <h3>Halaman home</h3>
+@endsection
